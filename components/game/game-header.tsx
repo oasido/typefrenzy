520675b700c98fc7644dbdf8f69da62e95732135
Ofek Asido <asido.ofek@gmail.com>
2022-06-13 17:25:31 +0300

    feat: :fire: add letter coloring to words if correct/wrong

--- components/game/game-header.tsx
+++ components/game/game-header.tsx
@@ -1,18 +1,32 @@
 import { Code, Container } from '@mantine/core';
 import { Text } from '@mantine/core';
-import { useEffect, useState } from 'react';
 import styles from '../../styles/game-header.module.css';
 import { useGameStore } from '../../context/useGameStore';
 
 export const GameHeader = () => {
-  const { isStarted, setIsStarted, word, getWord, setInput } = useGameStore();
-
-  const startGame = () => {};
+  const { isStarted, word, input } = useGameStore();
+  const wordArray: string[] = word.split('');
 
   return (
     <Container size="xs" className={styles.container}>
       {isStarted ? (
-        <h2 className={styles.word}>{word}</h2>
+        <h2 className={styles.word}>
+          {word.length > 0 &&
+            wordArray.map((letter, idx) => {
+              let color;
+              if (letter === input[idx] && word[idx] <= input[idx]) {
+                color = styles.correct;
+              } else if (idx < input.length) {
+                color = styles.incorrect;
+              }
+
+              return (
+                <span key={idx} className={color}>
+                  {letter}
+                </span>
+              );
+            })}
+        </h2>
       ) : (
         <Text size="xl" className={styles.start}>
           Type <Code>start</Code> to begin!
