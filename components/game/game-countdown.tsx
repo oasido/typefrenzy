eee83dc94ff4c25aa8e1d13bedf88e45f06582f8
Ofek Asido <asido.ofek@gmail.com>
2022-06-16 23:42:02 +0300

    feat: (wip) add countdown

---
+++ components/game/game-countdown.tsx
@@ -0,0 +1,60 @@
+import Countdown from 'react-countdown';
+import { Text } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { useGameStore } from '../../context/useGameStore';
+import { IoMdStopwatch } from 'react-icons/io';
+import styles from '../../styles/game-countdown.module.css';
+
+export const GameCountdown = () => {
+  const {
+    isInGame,
+    setIsInGame,
+    word,
+    setWord,
+    input,
+    gameStartDate,
+    isGameEnded,
+    setIsGameEnded,
+  } = useGameStore();
+  const [bonus, setBonus] = useState(0);
+
+  useEffect(() => {
+    if (input !== 'start' && isInGame === true) {
+      setBonus((prev) => prev + 1000);
+    }
+  }, [word]);
+
+  interface Irenderer {
+    minutes: number;
+    seconds: number;
+    milliseconds: number;
+  }
+  const renderer = ({ minutes, seconds, milliseconds }: Irenderer) => (
+    <Text>{`${minutes}:${seconds}.${milliseconds}s`}</Text>
+  );
+
+  const handleGameComplete = () => {
+    setIsInGame(false);
+    setIsGameEnded(true);
+    setBonus(0);
+    setWord('start');
+  };
+
+  return (
+    <>
+      {isInGame && !isGameEnded && (
+        <div className={styles.countdown}>
+          <IoMdStopwatch size={20} />
+          <Countdown
+            date={gameStartDate + 900000 + bonus}
+            intervalDelay={0}
+            precision={3}
+            onComplete={handleGameComplete}
+            renderer={renderer}
+            className={styles.countdown}
+          />
+        </div>
+      )}
+    </>
+  );
+};
