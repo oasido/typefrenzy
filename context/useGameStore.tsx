c6281a1030dd116e4002d170765b023acffdfcd1
Ofek Asido <asido.ofek@gmail.com>
2022-06-16 23:44:40 +0300

    feat: add more data in zustand context

--- context/useGameStore.tsx
+++ context/useGameStore.tsx
@@ -2,25 +2,35 @@ import create from 'zustand';
 import words from '../components/game/words.json';
 
 interface GameState {
-  isStarted: boolean;
-  setIsStarted: (boolean: boolean) => void;
+  isInGame: boolean;
+  setIsInGame: (boolean: boolean) => void;
+  isGameEnded: boolean;
+  setIsGameEnded: (boolean: boolean) => void;
   word: string;
   getWord: () => void;
+  setWord: (value: string) => void;
   input: string;
   setInput: (value: string) => void;
   answers: string[];
   setAnswers: (value: string) => void;
   removeAnswers: () => void;
+  gameStartDate: number;
+  setGameStartDate: (value: number) => void;
 }
 
 export const useGameStore = create<GameState>((set) => ({
-  isStarted: false,
-  setIsStarted: (boolean) => set(() => ({ isStarted: boolean })),
-  word: '',
+  isInGame: false,
+  setIsInGame: (boolean) => set(() => ({ isInGame: boolean })),
+  isGameEnded: false,
+  setIsGameEnded: (boolean) => set(() => ({ isGameEnded: boolean })),
+  word: 'start',
   getWord: () => set(() => ({ word: words[~~(Math.random() * words.length)] })),
+  setWord: (value) => set(() => ({ word: value })),
   input: '',
   setInput: (value) => set(() => ({ input: value })),
   answers: [],
   setAnswers: (value) => set((state) => ({ answers: [...state.answers, value] })),
   removeAnswers: () => set(() => ({ answers: [] })),
+  gameStartDate: Date.now(),
+  setGameStartDate: (date) => set(() => ({ gameStartDate: date })),
 }));
