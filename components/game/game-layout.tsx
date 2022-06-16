17f8674edbc8553818006f429d629670ded20824
Ofek Asido <asido.ofek@gmail.com>
2022-06-16 23:46:53 +0300

    feat: add new components

--- components/game/game-layout.tsx
+++ components/game/game-layout.tsx
@@ -1,13 +1,18 @@
-import { GameHeader } from './game-header';
+import { Container } from '@mantine/core';
+import { GameCountdown } from './game-countdown';
+import { GameStats } from './game-stats';
+import { GameWord } from './game-word';
 import { GameInput } from './game-input';
 import { GameAnswers } from './game-answers';
 
 export const GameLayout = (props: any) => {
   return (
-    <div>
-      <GameHeader />
+    <Container size="xs">
+      <GameCountdown />
+      <GameStats />
+      <GameWord />
       <GameInput />
       <GameAnswers />
-    </div>
+    </Container>
   );
 };
