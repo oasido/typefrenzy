647d8bb3c364775278b120613541edfcccc21535
Ofek Asido <asido.ofek@gmail.com>
2022-06-16 23:43:32 +0300

    refactor: remove container

--- components/game/game-answers.tsx
+++ components/game/game-answers.tsx
@@ -1,13 +1,8 @@
-import { Container } from '@mantine/core';
 import { useGameStore } from '../../context/useGameStore';
 import styles from '../../styles/game-answers.module.css';
 
 export const GameAnswers = () => {
   const { answers, setAnswers } = useGameStore();
 
-  return (
-    <Container size="xs" className={styles.container}>
-      <div className={styles.answers}>{answers.join(', ')}</div>
-    </Container>
-  );
+  return <div className={styles.answers}>{answers.join(', ')}</div>;
 };
