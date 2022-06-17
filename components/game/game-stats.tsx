7b3f725e09f35f198221adb0bd4b3110e550c037
Ofek Asido <asido.ofek@gmail.com>
2022-06-17 17:09:23 +0300

    feat: add game stats

--- components/game/game-stats.tsx
+++ components/game/game-stats.tsx
@@ -1,13 +1,38 @@
 import styles from '../../styles/game-stats.module.css';
 import { useGameStore } from '../../context/useGameStore';
+import { useEffect, useState } from 'react';
 
 export const GameStats = () => {
-  const { word, isInGame } = useGameStore();
+  const { answers } = useGameStore();
+  const [points, setPoints] = useState<number>(0);
+
+  const handlePoints = (): void => {
+    const wordLength = answers[answers.length - 1].length;
+
+    switch (true) {
+      case wordLength < 4: // less than 4 letters
+        setPoints(points + 1);
+        break;
+      case wordLength === 4: // 4 letters
+        setPoints(points + 2);
+        break;
+      case wordLength === 5: // 5 letters
+        setPoints(points + 3);
+        break;
+      case wordLength > 5: // more than 6 letters
+        setPoints(points + 4);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    answers.length > 0 && handlePoints();
+  }, [answers]);
 
   return (
     <div className={styles.stats}>
-      <div className={styles.stat}>points</div>
-      <div className={styles.stat}>words completed</div>
+      <div className={styles.stat}>{points} points</div>
+      <div className={styles.stat}>{answers.length} word count</div>
     </div>
   );
 };
