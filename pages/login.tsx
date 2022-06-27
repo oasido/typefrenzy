8cc9df9f7604ab2421a764acd531e124ef07db9f
Ofek Asido <asido.ofek@gmail.com>
2022-06-27 23:32:39 +0300

    feat: (wip) add basic login capability

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,15 +2,27 @@ import type { NextPage } from 'next';
 import { PageLayout } from '../components/page-layout/page-layout';
 import Link from 'next/link';
 import { useSession, signIn, signOut } from 'next-auth/react';
-import { Button, Center, Container, Input, Title, Text } from '@mantine/core';
+import { Button, Container, Input, Title, Text } from '@mantine/core';
 import { FaUserAstronaut } from 'react-icons/fa';
 import { MdOutlinePassword } from 'react-icons/md';
 import { RiFacebookCircleLine, RiGithubLine } from 'react-icons/ri';
 import styles from '../styles/login.module.css';
+import { useForm } from '@mantine/form';
 
 const Login: NextPage = () => {
   const { data: session } = useSession();
   console.log(session);
+
+  const form = useForm({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  const handleLogin = async () => {
+    signIn('credentials', { username: form.values.username, password: form.values.password });
+  };
 
   return (
     <PageLayout>
@@ -21,13 +33,22 @@ const Login: NextPage = () => {
             icon={<FaUserAstronaut className={styles.inputIcon} />}
             placeholder="username"
             size="lg"
+            name="username"
+            {...form.getInputProps('username')}
           />
           <Input
             icon={<MdOutlinePassword className={styles.inputIcon} />}
             placeholder="password"
             size="lg"
             type="password"
+            name="password"
+            {...form.getInputProps('password')}
           />
+
+          <Button color="green" onClick={handleLogin}>
+            Login
+          </Button>
+
           <Text>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
             Don't have an account?{' '}
