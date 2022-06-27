5389884fab7a93cb4ba2f49e706b052cdf6785eb
Ofek Asido <asido.ofek@gmail.com>
2022-06-27 03:37:26 +0300

    feat: :sparkles: add form validation with errors shown to client, if presenet

--- pages/register.tsx
+++ pages/register.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import { PageLayout } from '../components/page-layout/page-layout';
 import Link from 'next/link';
 import { useSession, signIn, signOut } from 'next-auth/react';
-import { Button, Center, Container, Input, Title, Text } from '@mantine/core';
+import { Button, Center, Container, Input, Title, Text, InputWrapper } from '@mantine/core';
 import { FaUserAstronaut } from 'react-icons/fa';
 import { MdOutlinePassword } from 'react-icons/md';
 import { RiFacebookCircleLine, RiGithubLine } from 'react-icons/ri';
@@ -10,13 +10,19 @@ import styles from '../styles/register.module.css';
 import { z } from 'zod';
 import { useForm, zodResolver } from '@mantine/form';
 
-const schema = z.object({
-  username: z
-    .string()
-    .min(2, { message: 'Username should have at least 2 letters' })
-    .max(15, { message: 'Username cannot be more than 15 characters' }),
-  password: z.string().min(6, { message: 'Password needs to be at least 6 characters' }),
-});
+const schema = z
+  .object({
+    username: z
+      .string()
+      .min(2, { message: 'Username should have at least 2 letters' })
+      .max(15, { message: 'Username cannot be more than 15 characters' }),
+    password: z.string().min(6, { message: 'Password needs to be at least 6 characters' }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
 
 const Register: NextPage = () => {
   const { data: session } = useSession();
@@ -38,23 +44,36 @@ const Register: NextPage = () => {
       <Container size="xl">
         <div className={styles.registerInputs}>
           <Title order={1}>Register</Title>
-          <Input
-            icon={<FaUserAstronaut className={styles.inputIcon} />}
-            placeholder="username"
-            size="lg"
-          />
-          <Input
-            icon={<MdOutlinePassword className={styles.inputIcon} />}
-            placeholder="password"
-            size="lg"
-            type="password"
-          />
-          <Input
-            icon={<MdOutlinePassword className={styles.inputIcon} />}
-            placeholder="password, again"
-            size="lg"
-            type="password"
-          />
+
+          <InputWrapper error={form.errors.username}>
+            <Input
+              icon={<FaUserAstronaut className={styles.inputIcon} />}
+              placeholder="username"
+              size="lg"
+              {...form.getInputProps('username')}
+            />
+          </InputWrapper>
+
+          <InputWrapper error={form.errors.password}>
+            <Input
+              icon={<MdOutlinePassword className={styles.inputIcon} />}
+              placeholder="password"
+              size="lg"
+              type="password"
+              {...form.getInputProps('password')}
+            />
+          </InputWrapper>
+
+          <InputWrapper error={form.errors.confirmPassword}>
+            <Input
+              icon={<MdOutlinePassword className={styles.inputIcon} />}
+              placeholder="password, again"
+              size="lg"
+              type="password"
+              {...form.getInputProps('confirmPassword')}
+            />
+          </InputWrapper>
+
           <Button color="green" onClick={handleRegister}>
             Register
           </Button>
