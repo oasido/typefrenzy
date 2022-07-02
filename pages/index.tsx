c79d47108337ab3e3e894c19b6daf77072f49c22
Ofek Asido <asido.ofek@gmail.com>
2022-07-02 19:27:07 +0300

    perf: remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next';
 import { GameLayout } from '../components/game/game-layout';
 import { PageLayout } from '../components/page-layout/page-layout';
-import { useSession, signIn, signOut } from 'next-auth/react';
-import { Button } from '@mantine/core';
 
 const Home: NextPage = () => {
   return (
