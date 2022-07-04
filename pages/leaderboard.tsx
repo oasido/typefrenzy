2840cac4db4b5ea28b6563e44b65cc714a0d7eb4
Ofek Asido <asido.ofek@gmail.com>
2022-07-04 22:25:15 +0300

    feat: (wip) get leaderboard

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,7 +1,23 @@
 import type { NextPage } from 'next';
+import { PageLayout } from '../components/page-layout/page-layout';
+import { Title } from '@mantine/core';
+import { supabase } from '../utils/supabaseClient';
 
 const Leaderboard: NextPage = () => {
-  return <div>About</div>;
+  return (
+    <PageLayout>
+      <Title>Leaderboard</Title>
+    </PageLayout>
+  );
 };
 
 export default Leaderboard;
+
+export const getServerSideProps = async () => {
+  // get users score from db
+  return {
+    props: {
+      // get list of users
+    },
+  };
+};
