25cf68b714f90b15662079b69789f55f2d3dc31e
Ofek Asido <asido.ofek@gmail.com>
2022-07-08 01:57:13 +0300

    feat: allow images from facebook and github

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['avatars.githubusercontent.com', 'platform-lookaside.fbsbx.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
