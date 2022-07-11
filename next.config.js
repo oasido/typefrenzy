dc4c0e49682fab502f5fe07a7e85ae4f387d1e62
Ofek Asido <asido.ofek@gmail.com>
2022-07-11 19:45:09 +0300

    feat: enable experimental standalone

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputStandalone: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['avatars.githubusercontent.com', 'platform-lookaside.fbsbx.com'],
